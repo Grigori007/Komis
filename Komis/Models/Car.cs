f343dec0dd2be1a5478fe511683a4106e6a4e60a
Grzegorz Kopeć <01113975@pw.edu.pl>
2019-02-21 19:50:28 +0100

    Added: Models file, Car class, ICarRepository, MockCarRepository

---
+++ Komis/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komis.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Mark  { get; set; }
+        public string Model { get; set; }
+        public int ProductionDate  { get; set; }
+        public string Mileage { get; set; }
+        public string EngineCapacity  { get; set; }
+        public string FuelType { get; set; }
+        public string Power { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string MiniImageUrl { get; set; }
+        public bool IsCarOfTheWeek { get; set; }
+    }
+
+
+}
---
+++ Komis/Models/MockCarRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komis.Models
+{
+    // Mock -> wersja probna do testowania
+    public class MockCarRepository : ICarRepository
+    {
+        private List<Car> _carsList;
+
+        public MockCarRepository()
+        {
+            if (_carsList == null)
+            {
+                LoadCarsToList();
+            }
+
+        }
+
+        private void LoadCarsToList()
+        {
+            _carsList = new List<Car>
+            {
+                new Car{CarId = 1, Mark="Toyota", Model="Corolla", ProductionDate=2001, Mileage="", EngineCapacity="2.1", FuelType="Diesel", Power="141", Price=5999, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=false},
+                new Car{CarId = 2, Mark="Renault", Model="Espace", ProductionDate=2009, Mileage="", EngineCapacity="", FuelType="", Power="", Price=, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=false},
+                new Car{CarId = 3, Mark="", Model="", ProductionDate=2000, Mileage="", EngineCapacity="", FuelType="", Power="", Price=40000, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=false},
+                new Car{CarId = 4, Mark="", Model="", ProductionDate=2000, Mileage="", EngineCapacity="", FuelType="", Power="", Price=40000, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=false},
+                new Car{CarId = 5, Mark="", Model="", ProductionDate=2000, Mileage="", EngineCapacity="", FuelType="", Power="", Price=40000, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=false},
+                new Car{CarId = 6, Mark="", Model="", ProductionDate=2000, Mileage="", EngineCapacity="", FuelType="", Power="", Price=40000, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=false},
+
+            };
+        }
+
+        public IEnumerable<Car> GetAllCars()
+        {
+            return _carsList;
+        }
+
+        public Car GetOneCarById(int carId)
+        {
+            return _carsList.FirstOrDefault(s => s.CarId == carId);
+        }
+    }
+}
