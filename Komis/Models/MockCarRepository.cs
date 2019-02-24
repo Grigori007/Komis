69e5129f36dea96661b15eb7066dde7e71683d6e
Grzegorz Kopeć <01113975@pw.edu.pl>
2019-02-24 17:59:33 +0100

    MockCarRepository changed

--- Komis/Models/MockCarRepository.cs
+++ Komis/Models/MockCarRepository.cs
@@ -21,7 +21,7 @@ namespace Komis.Models
         {
             _carsList = new List<Car>
             {
-                new Car{CarId = 1, Mark="Toyota", Model="Corolla", ProductionDate=2000, Mileage="298000 km", EngineCapacity="1400 cm3", FuelType="Petrol", Power="80", Price=2850, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=true, CarDescription="Opis 1"},
+                new Car{CarId = 1, Mark="Toyota", Model="Corolla", ProductionDate=2000, Mileage="298000 km", EngineCapacity="2000 cm3", FuelType="Diesel", Power="96", Price=5000, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=true, CarDescription="Opis 1"},
                 new Car{CarId = 2, Mark="Bentley", Model="Continental Flying Spur", ProductionDate=2009, Mileage="65000 km", EngineCapacity="6000 cm3", FuelType="Petrol", Power="610", Price=239000, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=false, CarDescription="Opis 2"},
                 new Car{CarId = 3, Mark="Warszawa", Model="224", ProductionDate=1970, Mileage="42000 km", EngineCapacity="2120 cm3", FuelType="Petrol", Power="70", Price=39900, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=false, CarDescription="Opis 3"},
                 new Car{CarId = 4, Mark="Maybach", Model="57", ProductionDate=2007, Mileage="96000 km", EngineCapacity="5998 cm3", FuelType="Petrol", Power="612", Price=399000, ImageUrl="", MiniImageUrl="", IsCarOfTheWeek=false, CarDescription="Opis 4"},
