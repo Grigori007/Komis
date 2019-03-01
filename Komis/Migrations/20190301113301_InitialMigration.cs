65cff219bec8d38e0d5a6b18ff0a04128654043c
Grzegorz Kopeć <01113975@pw.edu.pl>
2019-03-01 15:30:21 +0100

    DB early config

---
+++ Komis/Migrations/20190301113301_InitialMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Komis.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Mark = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    ProductionDate = table.Column<int>(nullable: false),
+                    Mileage = table.Column<string>(nullable: true),
+                    EngineCapacity = table.Column<string>(nullable: true),
+                    FuelType = table.Column<string>(nullable: true),
+                    Power = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    MiniImageUrl = table.Column<string>(nullable: true),
+                    IsCarOfTheWeek = table.Column<bool>(nullable: false),
+                    CarDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.CarId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
