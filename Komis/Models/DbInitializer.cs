22b0a4a7ad5f27598bbbb5a54f0f7d8e2df9c8c5
Grzegorz Kopeć <01113975@pw.edu.pl>
2019-03-01 16:29:06 +0100

    commqaqaqaqiiit.

---
+++ Komis/Models/DbInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komis.Models
+{
+    public static class DbInitializer
+    {
+        // przakazujemy tu AppDbContext. Inicjalizuje baze jeśli nie ma tam zanych danych.
+        // Metoda Seed bedzie wywolywana z klasy Program.cs
+        public static void Seed(AppDbContext context)
+        {
+            if (!context.Cars.Any())
+            {
+                // Zmiany sa dodawane do AppDbContext. Usuwamy ID bo baza dodaje je automatycznie
+                context.AddRange(
+                new Car { Mark = "Toyota", Model = "Corolla", ProductionDate = 2000, Mileage = "298000 km", EngineCapacity = "2000 cm3", FuelType = "Diesel", Power = "96", Price = 5000, ImageUrl = "", MiniImageUrl = "../images/corolla.jpg", IsCarOfTheWeek = true, CarDescription = "Opis 1" },
+                new Car { Mark = "Bentley", Model = "Continental Flying Spur", ProductionDate = 2009, Mileage = "65000 km", EngineCapacity = "6000 cm3", FuelType = "Petrol", Power = "610", Price = 239000, ImageUrl = "", MiniImageUrl = "../images/bentley flying spur.jpg", IsCarOfTheWeek = false, CarDescription = "Opis 2" },
+                new Car { Mark = "Warszawa", Model = "224", ProductionDate = 1970, Mileage = "42000 km", EngineCapacity = "2120 cm3", FuelType = "Petrol", Power = "70", Price = 39900, ImageUrl = "", MiniImageUrl = "../images/warszawa 224.jpg", IsCarOfTheWeek = false, CarDescription = "Opis 3" },
+                new Car { Mark = "Maybach", Model = "57", ProductionDate = 2007, Mileage = "96000 km", EngineCapacity = "5998 cm3", FuelType = "Petrol", Power = "612", Price = 399000, ImageUrl = "", MiniImageUrl = "../images/maybach 57.jpg", IsCarOfTheWeek = false, CarDescription = "Opis 4" },
+                new Car { Mark = "Ferrari", Model = "458 Italia", ProductionDate = 2012, Mileage = "20000 km", EngineCapacity = "4500 cm3", FuelType = "Petrol", Power = "570", Price = 714900, ImageUrl = "", MiniImageUrl = "../images/ferrari_458_italia.jpg", IsCarOfTheWeek = false, CarDescription = "Opis 5" },
+                new Car { Mark = "Jaguar", Model = "XJ X351", ProductionDate = 2017, Mileage = "26985 km", EngineCapacity = "2993 cm3", FuelType = "Diesel", Power = "300", Price = 177000, ImageUrl = "", MiniImageUrl = "../images/jaguar xj x351.jpg", IsCarOfTheWeek = false, CarDescription = "Opis 6" }
+                );
+            }
+            // Zapis danych z AppDbContext do bazy danych
+            context.SaveChanges();
+        }
+    }
+}
--- Komis/Program.cs
+++ Komis/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Komis.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Komis
@@ -15,7 +18,27 @@ namespace Komis
         public static void Main(string[] args)
         {
             // kreacja hosta
-            CreateWebHostBuilder(args).Build().Run();
+            // CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    // uzywamy to Dependency Injection by zyskac dostep do kontenera zaleznosci AppDbContext
+                    var context = services.GetRequiredService<AppDbContext>();
+                    context.Database.Migrate();
+                    DbInitializer.Seed(context);
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+
+            host.Run();
         }
 
         // konfiguracja hosta z pewnymi wstpenymi danymi. Odczytuje automatycznie plik appsettings.json
