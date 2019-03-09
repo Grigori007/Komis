e8160efe174c3faaede1c82500b9015c0acde481
Grzegorz Kopeć <01113975@pw.edu.pl>
2019-03-09 23:12:07 +0100

    Added LoginViewModel. Added Login view

---
+++ Komis/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komis.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityRole> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityRole> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Komis/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Komis.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name ="User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name ="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
